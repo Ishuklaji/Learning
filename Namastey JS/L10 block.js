bb37ae9ddbf6e60a98e1b21b22d574f057e3a608
Ishuklaji <ishuklaji100@gmail.com>
2023-02-27 22:49:35 +0530

    L10 Blocked Scope

---
+++ Namastey JS/L10 block.js
@@ -0,0 +1,14 @@
+if (true) {
+    var a = 10
+    console.log(a);
+}
+
+var a = 100
+{
+    var a = 10
+    var b = 20
+    var c = 30
+    console.log(a);
+    console.log(b)
+    console.log(c);
+}
