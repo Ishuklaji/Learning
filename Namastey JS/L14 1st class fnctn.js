75812f17f4c3f7b2d431d295b4f3d748ec450d0a
Ishuklaji <ishuklaji100@gmail.com>
2023-03-03 19:48:45 +0530

    1st class fn

---
+++ Namastey JS/L14 1st class fnctn.js
@@ -0,0 +1,26 @@
+// function statement aka function declaration
+function a() {
+    console.log("a called");
+}
+a()
+
+// function expression 
+var b = function () {
+    console.log("b called");
+}
+b()
+
+// anonymous function
+// function(){}
+
+// Named fn expression
+var c = function xyz() {
+    console.log("c called");
+}
+c()
+// xyz() //error
+
+// First class fn
+// the ability to use a function as value (arguments) inside another function is known as 1st class fn
+// first class citizens
+
