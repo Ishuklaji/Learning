9843401e31f6a9ee4f781a68efa349bbb88ef78b
Ishuklaji <ishuklaji100@gmail.com>
2023-03-13 18:54:11 +0530

    Promises

--- Namastey JS/L22 Promises.js
+++ Namastey JS/L22 Promises.js
@@ -9,3 +9,14 @@ const promise = createOrder(cart)
 promise.then(function (orderId) {
     proceedToPayment(orderId)
 })
+
+const GITHUB_API = "http://api.github.com/users/ishuklaji"
+
+const user = fetch(GITHUB_API)
+
+console.log(user);
+
+user.then(function (data) {
+    console.log(data);
+})
+
